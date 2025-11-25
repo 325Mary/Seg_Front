6a134d53fb16092759187654eaeed2b45c30a237
JEISON101 <165611721+JEISON101@users.noreply.github.com>
2025-11-24 23:05:55 -0500

    Update base URLs to use localhost for development
    Comment out previous API and documentation URLs.

--- src/GlobalConstanst copy.ts
+++ src/GlobalConstanst copy.ts
@@ -2,10 +2,3 @@
 // export const BaseUrlDocs  =  'http://localhost:3003/'
 
 // export const BaseUrlImages  =  'http://localhost:3003/'
-
-export const BaseUrlService  =  'http://localhost:5000/api/'
-export const BaseUrlDocs  =  'http://localhost:5000/'
-/* export const BaseUrlService  =  'https://seguimientosapi.cloudsenactpi.net/api/'
-export const BaseUrlDocs  =  'https://seguimientosapi.cloudsenactpi.net/' */
-
-export const BaseUrlImages  =  'https://seguimientosapi.cloudsenactpi.net/'
